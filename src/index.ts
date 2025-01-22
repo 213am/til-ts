6073be55d13c0ec69742f26eba3553b8e7051fd4
213am <dev.213am@gmail.com>
2025-01-22 09:58:33 +0900

    [docs] 기본형 타입 호환성

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,16 @@
-// 비동기 함수
-async function fetchGetTodo(): Promise<void> {
-  const res = await fetch("주소");
-}
+// 리터럴은 실제 값을 말한다
+// "hello" 는 "hello" 라는 문자열 리터럴이다
+const constStr: "hello" = "hello";
+// const 상수로 만들면 값은 "hello" 로 고정됨
 
-async function fetchGetTodoOne(): Promise<string> {
-  const res = await fetch("주소");
-  return "hello";
-}
+// "hello" 리터럴은 문자열에 포함된다(업캐스팅 된다)
+let str: string = constStr;
 
-async function fetchPostTodo(): Promise<boolean> {
-  const res = await fetch("주소");
-  return true;
-}
+// 리터럴로 표현하면
+let num: 100 = 100;
+// 100 리터럴은 숫자에 포함된다(업캐스팅 된다)
+let num2: number = num;
 
-type Todo = {
-  id: number;
-  title: string;
-};
-async function fetchSortTodo(): Promise<Todo> {
-  const res = await fetch("주소");
-  return { id: 1, title: "안녕" };
-}
+// false 리터럴은 불리언에 포함된다(업캐스팅 된다)
+const isLive: false = false;
+let isLive2: boolean = isLive;
---
+++ src/sample.ts
@@ -0,0 +1,7 @@
+interface sampleProps {
+  // Add your props here
+}
+const sample: React.FC<sampleProps> = () => {
+  return <div>sample</div>;
+};
+export default sample;
