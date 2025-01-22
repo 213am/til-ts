2bca5fdc0e74a8e357be781ab4b65536359ccd43
213am <dev.213am@gmail.com>
2025-01-22 11:10:39 +0900

    [docs] 객체 타입의 호환성

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
-// 리터럴은 실제 값을 말한다
-// "hello" 는 "hello" 라는 문자열 리터럴이다
-const constStr: "hello" = "hello";
-// const 상수로 만들면 값은 "hello" 로 고정됨
+type Person = { name: string };
+type Employ = { company: string };
+type Sample = Person & Employ;
 
-// "hello" 리터럴은 문자열에 포함된다(업캐스팅 된다)
-let str: string = constStr;
-
-// 리터럴로 표현하면
-let num: 100 = 100;
-// 100 리터럴은 숫자에 포함된다(업캐스팅 된다)
-let num2: number = num;
-
-// false 리터럴은 불리언에 포함된다(업캐스팅 된다)
-const isLive: false = false;
-let isLive2: boolean = isLive;
+// 속성이 하나만 없어도 타입오류
+const whoa: Sample = { name: "hong" }; // 타입 오류
+const whob: Sample = { company: "green" }; // 타입 오류
+// Sample 타입은 Person 과 Employ 모두를 만족하는 타입이므로
+// 두 타입 모두의 속성을 가지고 있어야 한다.
+const whoc: Sample = { name: "hong", company: "green" };
